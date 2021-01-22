0a790464bdd4d55e4f3d73c8803bdc6bc45b2ac2
jan <janvanbergen16@gmail.com>
2021-01-22 21:56:13 +0100

    Stereo

--- main.cpp
+++ main.cpp
@@ -26,6 +26,11 @@ static float play_square(float t, float freq, float amplitude = 1.0f) {
 	return std::fmodf(t * freq, 1.0f) < 0.5f ? amplitude : -amplitude;
 }
 
+static float play_triangle(float t, float freq, float amplitude = 1.0f) {
+	float x = t * freq + 0.25f;
+	return amplitude * (4.0f * std::abs(x - std::floor(x + 0.5f)) - 1.0f);
+}
+
 static float t = 0.0f;
 
 static Uint64 time_last;
@@ -33,9 +38,9 @@ static float  time_inv_freq;
 
 static void sdl_audio_callback(void * user_data, Uint8 * stream, int len) {
 	int jakob[] = { 
+		0,0, 2,2, 4,4, 0,0,
 		0,0, 2,2, 4,4, 0,0, 
-		0,0, 2,2, 4,4, 0,0, 
-		4,4, 5,5, 7,7, 7,7, 
+		4,4, 5,5, 7,7, 7,7,
 		4,4, 5,5, 7,7, 7,7, 
 		7,9, 7,5, 4,4, 0,0,
 		7,9, 7,5, 4,4, 0,0,
@@ -43,12 +48,17 @@ static void sdl_audio_callback(void * user_data, Uint8 * stream, int len) {
 		0,0, -5,-5, 0,0, 0,0
 	};
 
-	for (int i = 0; i < len; i++) {
-		stream[i] = 
-			char(play_square(t, note_freq(36 + jakob[(int(t * 2))      % 64]), 48.0f)) +
-			char(play_square(t, note_freq(24 + jakob[(int(t * 2) + 16) % 64]), 32.0f)) + 
-			char(play_square(t, note_freq(12 + jakob[(int(t * 2) + 32) % 64]), 36.0f));
+	printf("%i\n", len);
 
+	for (int i = 0; i < len; i += 2) {
+		auto val = 
+			char(play_square  (t, note_freq(36 + jakob[(int(t * 2))      % 64]), 48.0f)) +
+			char(play_saw     (t, note_freq(24 + jakob[(int(t * 2) + 16) % 64]), 32.0f)) + 
+			char(play_triangle(t, note_freq(12 + jakob[(int(t * 2) + 32) % 64]), 36.0f));
+
+		stream[i]     = val;
+		stream[i + 1] = val;
+		
 		t += 1.0f / float(SAMPLE_RATE);
 	}
 }
@@ -58,8 +68,8 @@ int main(int argc, char * argv[]) {
 
 	SDL_AudioSpec audio_spec = { };
 	audio_spec.freq     = SAMPLE_RATE;
-	audio_spec.format   = AUDIO_S16SYS;
-	audio_spec.channels = 1;
+	audio_spec.format   = AUDIO_S8;
+	audio_spec.channels = 2;
 	audio_spec.samples  = 1024;
 	audio_spec.callback = sdl_audio_callback;
 
@@ -68,10 +78,9 @@ int main(int argc, char * argv[]) {
 	time_inv_freq = 1.0f / float(SDL_GetPerformanceFrequency());
 	time_last     = SDL_GetPerformanceCounter();
 
-	// unpausing the audio device (starts playing):
 	SDL_PauseAudioDevice(device, 0);
 
-	SDL_Delay(30000);
+	while (true) SDL_Delay(1000);
 
 	SDL_CloseAudioDevice(device);
 	SDL_Quit();
