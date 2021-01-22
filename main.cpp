4d75cbcad07f2b720cfe16ce1ad0480bee7760e7
jan <janvanbergen16@gmail.com>
2021-01-22 20:48:11 +0100

    Saw + square wave

--- main.cpp
+++ main.cpp
@@ -4,24 +4,50 @@
 
 #include <SDL2/SDL.h>
 
-constexpr float TWO_PI = 6.28318530718f;
+static constexpr float TWO_PI = 6.28318530718f;
 
-constexpr float A_0 = 27.5f; // Tuning of A 0 in Hz
+static constexpr float A_0 = 27.5f; // Tuning of A 0 in Hz
 
-float note_freq(int note) {
+static constexpr int SAMPLE_RATE = 44100;
+
+static float note_freq(int note) {
 	return A_0 * std::pow(2.0f, float(note) / 12.0f);
 }
 
-constexpr int SAMPLE_RATE = 44100;
+static float play_sine(float t, float freq, float amplitude = 1.0f) {
+	return amplitude * std::sin(TWO_PI * t * freq);
+}
+
+static float play_saw(float t, float freq, float amplitude = 1.0f) {
+	return amplitude * 2.0f * (t*freq - std::floor(t*freq + 0.5f));
+}
+
+static float play_square(float t, float freq, float amplitude = 1.0f) {
+	return std::fmodf(t * freq, 1.0f) < 0.5f ? amplitude : -amplitude;
+}
 
 static float t = 0.0f;
 
-void sdl_audio_callback(void * user_data, Uint8 * stream, int len) {
-	int jakob[] = { 0, 2, 4, 0, 0, 2, 4, 0, 4, 5, 7, 7, 4, 5, 7, 7 };
+static Uint64 time_last;
+static float  time_inv_freq;
+
+static void sdl_audio_callback(void * user_data, Uint8 * stream, int len) {
+	int jakob[] = { 
+		0,0, 2,2, 4,4, 0,0, 
+		0,0, 2,2, 4,4, 0,0, 
+		4,4, 5,5, 7,7, 7,7, 
+		4,4, 5,5, 7,7, 7,7, 
+		7,9, 7,5, 4,4, 0,0,
+		7,9, 7,5, 4,4, 0,0,
+		0,0, -5,-5, 0,0, 0,0,
+		0,0, -5,-5, 0,0, 0,0
+	};
 
 	for (int i = 0; i < len; i++) {
-		char sample = std::sin(TWO_PI * t * note_freq(36 + jakob[int(t) % 16])) * 512.0f;
-		stream[i] = sample / 4;
+		stream[i] = 
+			char(play_square(t, note_freq(36 + jakob[(int(t * 2))      % 64]), 48.0f)) +
+			char(play_square(t, note_freq(24 + jakob[(int(t * 2) + 16) % 64]), 32.0f)) + 
+			char(play_square(t, note_freq(12 + jakob[(int(t * 2) + 32) % 64]), 36.0f));
 
 		t += 1.0f / float(SAMPLE_RATE);
 	}
@@ -38,6 +64,9 @@ int main(int argc, char * argv[]) {
 	audio_spec.callback = sdl_audio_callback;
 
 	auto device = SDL_OpenAudioDevice(nullptr, 0, &audio_spec, nullptr, 0);
+
+	time_inv_freq = 1.0f / float(SDL_GetPerformanceFrequency());
+	time_last     = SDL_GetPerformanceCounter();
 
 	// unpausing the audio device (starts playing):
 	SDL_PauseAudioDevice(device, 0);
